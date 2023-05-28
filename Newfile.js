2fa7136050686e331382bcb834c64eed16812e7a
tejassawadh183 <sawadhtejas183@gmail.com>
2023-05-28 15:13:48 +0530

    updation

--- Newfile.js
+++ Newfile.js
@@ -4,16 +4,29 @@
 // console.log(document.title)
 // console.log(document.all)
 // console.log(document.all[10])
-console.log(document.head)
-console.log(document.body)
-let header = document.getElementById('header-title');
-console.log(header)
+// console.log(document.head)
+// console.log(document.body)
 
+// Get element By Id
 
-
+// let header = document.getElementById('header-title');
+// console.log(header)
 // header.textContent = "Hello";
 // header.innerText = "Good bye"
-
 // header.style.borderBottom ='solid 3px black'
-var input = document.getElementById('form-inline')
-console.log(input);
+// var input = document.getElementById('form-inline')
+// console.log(input);
+
+
+//Get elements by class
+var items = document.getElementsByClassName('list-group-item')
+console.log(items);
+console.log(items[1]);
+items[1].textContent ='Item No 2';
+// items[1].style.fontWeight = 'bold'
+items[2].style.backgroundColor = 'Green'
+
+for(var i =0; i<items.length; i++)
+{
+    items[i].style.fontWeight = 'bold'
+}
