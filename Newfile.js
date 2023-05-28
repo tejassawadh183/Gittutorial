025d0abc16000e736769c0170a1ceb5caca2881b
tejassawadh183 <sawadhtejas183@gmail.com>
2023-05-28 15:20:28 +0530

    update

--- Newfile.js
+++ Newfile.js
@@ -19,14 +19,30 @@
 
 
 //Get elements by class
-var items = document.getElementsByClassName('list-group-item')
-console.log(items);
-console.log(items[1]);
-items[1].textContent ='Item No 2';
-// items[1].style.fontWeight = 'bold'
-items[2].style.backgroundColor = 'Green'
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent ='Item No 2';
+// // items[1].style.fontWeight = 'bold'
+// items[2].style.backgroundColor = 'Green'
 
-for(var i =0; i<items.length; i++)
+// for(var i =0; i<items.length; i++)
+// {
+//     items[i].style.fontWeight = 'bold'
+// }
+
+
+
+// Get Element by Tag name
+var li = document.getElementsByTagName('li');
+
+console.log(li);
+console.log(li[1]);
+li[1].textContent ='Item No 2';
+// items[1].style.fontWeight = 'bold'
+li[2].style.backgroundColor = 'Green'
+
+for(var i =0; i<li.length; i++)
 {
-    items[i].style.fontWeight = 'bold'
+    li[i].style.fontWeight = 'bold'
 }
