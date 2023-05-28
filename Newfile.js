df6e65e538467f54cca31c88c481bf3131c5fa3b
tejassawadh183 <sawadhtejas183@gmail.com>
2023-05-28 14:32:46 +0530

    adding new file

---
+++ Newfile.js
@@ -0,0 +1,19 @@
+// console.log(document.applets)
+// console.log(document.URL)
+// document.title = "item lister2"
+// console.log(document.title)
+// console.log(document.all)
+// console.log(document.all[10])
+console.log(document.head)
+console.log(document.body)
+let header = document.getElementById('header-title');
+console.log(header)
+
+
+
+// header.textContent = "Hello";
+// header.innerText = "Good bye"
+
+// header.style.borderBottom ='solid 3px black'
+var input = document.getElementById('form-inline')
+console.log(input);
